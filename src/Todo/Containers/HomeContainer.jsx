123902884fe4d9841ec2042ca531ef82f63875c0
Chetan Vihite <cvihite@wwnorton.com>
2019-01-18 16:34:57 -0500

    added removeTodo

--- src/Todo/Containers/HomeContainer.jsx
+++ src/Todo/Containers/HomeContainer.jsx
@@ -27,7 +27,7 @@ class HomeContainer extends Component {
           "status": true
         }, {
           "id": 4,
-          "whatToDo": "Finish something",
+          "whatToDo": "Read something",
           "assignedTo": "Randee Ramshaw",
           "status": true
         }, {
@@ -36,34 +36,72 @@ class HomeContainer extends Component {
           "assignedTo": "Nealy Rizzetti",
           "status": false
         }]
-		}
+    }
+    
+    this.addNewTodo = this.addNewTodo.bind(this);
+    this.removeTodo = this.removeTodo.bind(this);
 	}
-
-	
+  componentDidMount(){
+    // load data from -- https://my.api.mockaroo.com/todos.json?key=0b8e0b60
+    console.log (this.state.todos);
+  }
+	addNewTodo = (e) => {
+    let emptyTodo = {
+      id: Math.floor(Math.random() * 100 ),
+      whatToDo: '',
+      assignedTo: '',
+      status: false
+    }
+    let currentTodos = this.state.todos;
+    currentTodos.push(emptyTodo);
+    this.setState ({todos: currentTodos});
+  }
+  removeTodo = (e) => {
+    let currentTodos = this.state.todos;
+    let index = currentTodos.findIndex( x => x.id === e.id);
+    if ( index !== -1) {//not found 
+      currentTodos.splice(index, 1);
+      this.setState({todos: currentTodos});
+    }
+  }
 	render () {
     let todos = this.state.todos;
-
+    let removeTodo = this.removeTodo;
 		return (
 			<div>
         <div className="header">
           Welcome to my Todo App
         </div>
+        <div>
+        <div className="headerRow">
+              <div className="itemStyle">What</div>
+              <div className="itemStyle">Who</div>
+              <div className="removeCellStyle">Complete?</div>
+              <div className="removeCellStyle"></div>
+            </div>
+        </div>
         {
           todos && todos.map(function(item, index){      
           return (
-            <div className="todoRow">
-              <div className="itemStyle">{item.whatToDo}</div>
-              <div className="itemStyle">{item.assignedTo}</div>
-              <div className="removeCellStyle">{item.status ? 'Completed' : 'In Process'}</div>
+            <div className="todoRow" key={index}>
+              <div className="itemStyle">
+                <input type="text" className="inputStyle" value={item.whatToDo}></input> 
+              </div>
+              <div className="itemStyle">
+                <input type="text" className="inputStyle" value={item.assignedTo}></input> 
+              </div>
+              <div className="removeCellStyle">{item.status ? 'Complete' : 'In Process'}</div>
               <div className="removeCellStyle">
-                <img src={removeIcon} className="deleteIcon" alt="remove" />
+                <button className="noborderButton" onClick={() => removeTodo(item)}>
+                  <img src={removeIcon} className="deleteIcon" alt="remove" />
+                </button>
               </div>
             </div>);
         })
 
         }
         <div className="floatRight">
-          <a href="#" onClick={this.addTodo} >Add New Todo</a>
+          <button className="buttonStyle" onClick={this.addNewTodo} >Add New Todo</button>
         </div>
 			</div>
 		)
